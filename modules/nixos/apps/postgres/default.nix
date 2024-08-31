45e06b6e5c085556338d716c892c483b6b508716
Abdallah Ali <amaali1991@gmail.com>
2024-08-31 13:35:33 +0300

    add smoe security configuration postgresql

--- modules/nixos/apps/postgres/default.nix
+++ modules/nixos/apps/postgres/default.nix
@@ -13,6 +13,22 @@ in {
       dataDir = "/data/pg";
       enable = true;
       enableTCPIP = true;
+      ensureDatabases = [ "a7db" ];
+      identMap = ''
+        					# ArbitraryMapName systemUser DBUser
+        						 superuser_map      root      postgres
+        						 superuser_map      postgres  postgres
+        						 # Let other names login as themselves
+        						 superuser_map      /^(.*)$   \1
+        				'';
+      authentication = pkgs.lib.mkOverride 10 ''
+        					#...
+        					#type database DBuser origin-address auth-method
+        					# ipv4
+        					host  all      all     127.0.0.1/32   trust
+        					# ipv6
+        					host all       all     ::1/128        trust
+        				'';
       settings = {
         port = 5050;
         log_min_messages = "warning";
