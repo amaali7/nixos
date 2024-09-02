c35e2ebfafa891bb9e3fab2c385f3eedb3a7e302
Abdallah Ali <amaali1991@gmail.com>
2024-09-02 11:34:33 +0300

    add lix

--- flake.nix
+++ flake.nix
@@ -11,6 +11,11 @@
       url = "github:amaali7/kde-material-you-colors-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+		# Lix
+    lix = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # Home Manager (release-22.05)
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -170,6 +175,7 @@
         flake.overlays.default
         # snowfall-docs.overlays
         nixgl.overlay
+        lix.overlay.default
         nixpkgs-f2k.overlays.default
         yazi.overlays.default
       ];
@@ -178,6 +184,7 @@
         home-manager.nixosModules.home-manager
         nur.nixosModules.nur
         nix-ld.nixosModules.nix-ld
+        lix.nixosModules.default
         # @TODO(jakehamilton): Replace amaali7.services.attic now that vault-agent
         # exists and can force override environment files.
         # attic.nixosModules.atticd
--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -15,7 +15,7 @@ in
 {
   options.amaali7.nix = with types; {
     enable = mkBoolOpt true "Whether or not to manage nix configuration.";
-    package = mkOpt package pkgs.nixVersions.latest "Which nix package to use.";
+    package = mkOpt package pkgs.lix "Which nix package to use.";
 
     default-substituter = {
       url = mkOpt str "https://cache.nixos.org" "The url for the substituter.";
