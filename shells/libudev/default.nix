d0e0ba041c8591468265fa7f274880239f8b598a
Abdallah Ali <amaali1991@gmail.com>
2024-09-18 22:28:21 +0300

    same

--- shells/libudev/default.nix
+++ shells/libudev/default.nix
@@ -6,12 +6,15 @@
 mkShell {
   packages = with pkgs; [
     libudev-zero
+    openssl
+    pkg-config
   ];
 
   shellHook = ''
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
-              pkgs.udev
-              pkgs.libudev-zero
-            ]}"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+      pkgs.udev
+      pkgs.libudev-zero
+      pkgs.openssl
+    ]}"
   '';
 }
