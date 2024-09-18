cd8b3d4b45bf996aa18ddde20cf281f1b42ffd44
Abdallah Ali <amaali1991@gmail.com>
2024-09-18 21:00:36 +0300

    add esp-shell

---
+++ shells/esp-shell/default.nix
@@ -0,0 +1,19 @@
+{ pkgs
+, mkShell
+, ...
+}:
+
+mkShell {
+  packages = with pkgs; [
+    openssl
+    cargo-xbuild
+    rust-bindgen
+    cargo-espflash
+  ];
+
+  shellHook = ''
+  export DEBUG=1
+  export PATH="/home/ai3wm/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
+  export LIBCLANG_PATH="/home/ai3wm/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-17.0.1_20240419/esp-clang/lib"
+'';
+}
