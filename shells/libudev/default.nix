9b2c3ccdd4e958315854d01837686fb285dd6442
Abdallah Ali <amaali1991@gmail.com>
2024-09-18 22:26:13 +0300

    add lib path to libudev shell

--- shells/libudev/default.nix
+++ shells/libudev/default.nix
@@ -9,5 +9,9 @@ mkShell {
   ];
 
   shellHook = ''
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+              pkgs.udev
+              pkgs.libudev-zero
+            ]}"
   '';
 }
