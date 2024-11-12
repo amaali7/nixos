05025d38d72989ba596798fc422e2b730ff82874
Abdallah Ali <amaali1991@gmail.com>
2024-11-12 23:05:17 +0200

    libudev fix 3

--- shells/libudev/default.nix
+++ shells/libudev/default.nix
@@ -10,7 +10,7 @@ mkShell {
     openssl
     pkg-config
   ];
-
+  OPENSSL_NO_VENDOR = 1;
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
       pkgs.udev
