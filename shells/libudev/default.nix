17bcde6f0ad88320a7bca7dea746ee06a256b813
Abdallah Ali <amaali1991@gmail.com>
2024-11-12 23:00:06 +0200

    fix libudev shell

--- shells/libudev/default.nix
+++ shells/libudev/default.nix
@@ -12,9 +12,10 @@ mkShell {
 
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
-      pkgs.udev
-      pkgs.libudev-zero
+      pkgs.udev.dev
+      pkgs.libudev-zero.out
       pkgs.openssl
+      pkgs.eudev.out 
     ]}"
   '';
 }
