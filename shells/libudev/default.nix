468fe49d47ad0e7b1b5c6cf704bca55d04ad04da
Abdallah Ali <amaali1991@gmail.com>
2024-12-09 05:10:11 +0200

    remove espflash and modify udev shell

--- shells/libudev/default.nix
+++ shells/libudev/default.nix
@@ -1,22 +1,11 @@
-{ pkgs
-, mkShell
-, ...
-}:
+{ pkgs, mkShell, ... }:
 
 mkShell {
-  packages = with pkgs; [
-    libudev-zero
-    udev
-    openssl
-    pkg-config
-  ];
+  packages = with pkgs; [ udev openssl pkg-config ];
   OPENSSL_NO_VENDOR = 1;
   shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
-      pkgs.udev
-      pkgs.libudev-zero
-      pkgs.openssl
-      pkgs.eudev.out 
-    ]}"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
+      pkgs.lib.makeLibraryPath [ pkgs.udev pkgs.openssl ]
+    }"
   '';
 }
