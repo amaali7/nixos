38d856d22e0df99185e835f4931ade48239d4b87
Abdallah Ali <amaali1991@gmail.com>
2025-01-12 03:21:34 +0200

    enable rootless docker

--- modules/nixos/apps/docker/default.nix
+++ modules/nixos/apps/docker/default.nix
@@ -8,7 +8,13 @@ in {
   };
 
   config = mkIf cfg.enable {
-    virtualisation.docker = { enable = true; };
+    virtualisation.docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
     environment.systemPackages = with pkgs; [ docker-compose lazydocker ];
   };
 }
