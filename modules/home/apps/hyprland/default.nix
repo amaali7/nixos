8526f580868947130c5c8b0fb27f86b3d30e04a4
Abdallah Ali <amaali1991@gmail.com>
2025-02-12 21:31:23 +0200

    disable hypr settings from home manager

--- modules/home/apps/hyprland/default.nix
+++ modules/home/apps/hyprland/default.nix
@@ -85,23 +85,6 @@ in {
       #   inputs.hyprland-plugins.packages.${pkgs.system}.csgo-vulkan-fix
       #   inputs.hyprland-plugins.packages.${pkgs.system}.xtra-dispatchers
       # ];
-
-      settings = {
-        "$mod" = "SUPER";
-        bind = [
-          "$mod, ENTER, exec, kitty"
-          "$mod, F, exec, firefox"
-          ", Print, exec, grimblast copy area"
-        ] ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]) 9));
-      };
     };
     # home.file."${config.xdg.configHome}" = {
     #   source = config.lib.file.mkOutOfStoreSymlink "${homeD}/.dotfiles/config/";
