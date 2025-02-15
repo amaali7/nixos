b9b4ff146a549e6c9adb8a18dfea60f8a20e7c39
Abdallah Ali <amaali1991@gmail.com>
2025-02-15 17:04:10 +0200

    add aya rs shell

---
+++ shells/rust/aya-rs/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, mkShell, ... }:
+let
+  aya_run = pkgs.writeShellScriptBin "aya-run" ''
+    RUST_LOG=info cargo run --config 'target."cfg(all())".runner="sudo -E"' -- \
+      --iface $1'';
+in mkShell {
+  # The Nix packages provided in the environment
+  packages = (with pkgs; [ aya_run bpftools inetutils libllvm ])
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
+
+  RUST_LOG = "info";
+  RUST_BACKTRACE = 1;
+  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  shellHook = ''
+    # Required
+    echo "Welcome to nix Aya Rust Shell"
+  '';
+}
