f07287f6bdc1434d032b6d9db0b7842a715cd71d
Abdallah Ali <amaali1991@gmail.com>
2025-02-27 00:49:19 +0200

    freetube last version

--- overlays/freetube/default.nix
+++ overlays/freetube/default.nix
@@ -1,23 +1,24 @@
-{ channels, ... }:
-
-final: prev:
-
-{
-  inherit (channels.unstable) freetube;
-}
 # { channels, ... }:
 
-# final: prev: {
-#   freetube = prev.freetube // rec{
-#     version = "0.23.2";
+# final: prev:
 
-#     src = fetchFromGitHub {
-#       owner = "FreeTubeApp";
-#       repo = "FreeTube";
-#       tag = "v${version}-beta";
-#       hash = "sha256-I3C6gPPSTD/GzCoyyAxTfNEW83etJKJFb3uUKLUT79c=";
-#     };
-#   };
-
+# {
 #   inherit (channels.unstable) freetube;
 # }
+{ channels, ... }:
+
+final: prev: {
+
+  #inherit (channels.unstable) freetube;
+  freetube = prev.freetube // rec {
+    version = "0.23.2";
+
+    src = prev.fetchFromGitHub {
+      owner = "FreeTubeApp";
+      repo = "FreeTube";
+      tag = "v${version}-beta";
+      hash = "sha256-P0ENx8PDWbqfiBEsWv11R3Q/FE+rAFhhk49VyQgXIz4=";
+    };
+  };
+
+}
