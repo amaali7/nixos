4745c9e026e946f2527e997eebed9e29a5fb26ad
Abdallah Ali <amaali1991@gmail.com>
2025-03-11 10:12:32 +0200

    tray fix gpg

--- modules/nixos/security/gpg/default.nix
+++ modules/nixos/security/gpg/default.nix
@@ -1,30 +1,79 @@
-{ options, config, pkgs, lib, inputs, ... }:
-
+{ options, config, pkgs, lib, inputs, namespace, ... }:
 with lib;
-with lib.amaali7;
-let cfg = config.amaali7.security.gpg;
+with lib.${namespace};
+let
+  cfg = config.${namespace}.security.gpg;
+
+  gpgConf = "${inputs.gpg-base-conf}/gpg.conf";
+
+  gpgAgentConf = ''
+    enable-ssh-support
+    default-cache-ttl 60
+    max-cache-ttl 120
+    pinentry-program ${pkgs.pinentry-gnome3}/bin/pinentry-gnome3
+  '';
+
+  theme = pkgs.fetchFromGitHub {
+    owner = "jez";
+    repo = "pandoc-markdown-css-theme";
+    rev = "019a4829242937761949274916022e9861ed0627";
+    sha256 = "1h48yqffpaz437f3c9hfryf23r95rr319lrb3y79kxpxbc9hihxb";
+  };
 in {
-  options.amaali7.security.gpg = with types; {
+  options.${namespace}.security.gpg = with types; {
     enable = mkBoolOpt false "Whether or not to enable GPG.";
+    agentTimeout = mkOpt int 5
+      "The amount of time to wait before continuing with shell init.";
   };
 
   config = mkIf cfg.enable {
+    services.pcscd.enable = true;
+
+    # NOTE: This should already have been added by programs.gpg, but
+    # keeping it here for now just in case.
+    environment.shellInit = ''
+      export GPG_TTY="$(tty)"
+      export SSH_AUTH_SOCK=$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)
+
+      ${pkgs.coreutils}/bin/timeout ${
+        builtins.toString cfg.agentTimeout
+      } ${pkgs.gnupg}/bin/gpgconf --launch gpg-agent
+      gpg_agent_timeout_status=$?
+
+      if [ "$gpg_agent_timeout_status" = 124 ]; then
+        # Command timed out...
+        echo "GPG Agent timed out..."
+        echo 'Run "gpgconf --launch gpg-agent" to try and launch it again.'
+      fi
+    '';
+
     environment.systemPackages = with pkgs; [
       cryptsetup
       paperkey
       gnupg
       pinentry-curses
       pinentry-qt
+      pinentry-gnome3
       paperkey
     ];
 
     programs = {
       ssh.startAgent = false;
+
       gnupg.agent = {
         enable = true;
         enableSSHSupport = true;
         enableExtraSocket = true;
-        # pinentryFlavor = "gnome3";
+        pinentryPackage = pkgs.pinentry-gnome3;
+      };
+    };
+
+    amaali7 = {
+      home.file = {
+        ".gnupg/.keep".text = "";
+
+        ".gnupg/gpg.conf".source = gpgConf;
+        ".gnupg/gpg-agent.conf".text = gpgAgentConf;
       };
     };
   };
