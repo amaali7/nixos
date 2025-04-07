9d67d2599a2351eb09ea2a90f69210d610b15122
Abdallah Ali <amaali1991@gmail.com>
2025-04-07 06:13:05 +0200

    use doas aya-rs

--- shells/rust/aya-rs/default.nix
+++ shells/rust/aya-rs/default.nix
@@ -2,39 +2,39 @@
 let
   netns_up = pkgs.writeShellScriptBin "inet-up" ''
     # Create namespaces
-    sudo ip netns add red
-    sudo ip netns add blue
+    doas ip netns add red
+    doas ip netns add blue
 
     # Create veth pair (veth-red <--> veth-blue)
-    sudo ip link add veth-red type veth peer name veth-blue
+    doas ip link add veth-red type veth peer name veth-blue
 
     # Move interfaces to respective namespaces
-    sudo ip link set veth-red netns red
-    sudo ip link set veth-blue netns blue
+    doas ip link set veth-red netns red
+    doas ip link set veth-blue netns blue
 
     # Assign IPs (different subnets for isolation)
-    sudo ip -n red addr add 192.168.1.1/24 dev veth-red
-    sudo ip -n blue addr add 192.168.2.1/24 dev veth-blue
+    doas ip -n red addr add 192.168.1.1/24 dev veth-red
+    doas ip -n blue addr add 192.168.2.1/24 dev veth-blue
 
     # Bring them up
-    sudo ip -n red link set veth-red up
-    sudo ip -n blue link set veth-blue up
+    doas ip -n red link set veth-red up
+    doas ip -n blue link set veth-blue up
 
     # Enable loopback in both namespaces
-    sudo ip -n red link set lo up
-    sudo ip -n blue link set lo up
+    doas ip -n red link set lo up
+    doas ip -n blue link set lo up
 
     # Ping from red to blue (should fail - no route)
-    sudo ip netns exec red ping 192.168.2.1 -c 3
+    doas ip netns exec red ping 192.168.2.1 -c 3
 
     # Ping from blue to red (should fail)
-    sudo ip netns exec blue ping 192.168.1.1 -c 3
+    doas ip netns exec blue ping 192.168.1.1 -c 3
 
   '';
   netns_down = pkgs.writeShellScriptBin "inet-down" ''
     # Delete namespaces (automatically removes interfaces)
-    sudo ip netns del red
-    sudo ip netns del blue
+    doas ip netns del red
+    doas ip netns del blue
   '';
   aya_run = pkgs.writeShellScriptBin "aya-run" ''
     RUST_LOG=info cargo run --config 'target."cfg(all())".runner="doas "' -- \
@@ -43,6 +43,7 @@ in mkShell {
   # The Nix packages provided in the environment
   packages = with pkgs;
     [
+      doas
       aya_run
       netns_up
       netns_down
