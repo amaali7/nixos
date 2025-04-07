2cda3f1103ec7a07563f4ae32be440304873d344
Abdallah Ali <amaali1991@gmail.com>
2025-04-07 06:43:37 +0200

    use dummy interface for testing

--- shells/rust/aya-rs/default.nix
+++ shells/rust/aya-rs/default.nix
@@ -1,44 +1,31 @@
 { pkgs, mkShell, inputs, ... }:
 let
-  netns_up = pkgs.writeShellScriptBin "inet-up" ''
-    # Create namespaces
-     ip netns add red
-     ip netns add blue
+  netns_up = pkgs.writeShellScriptBin "dnet-up" ''
+    # Load the dummy kernel module (if not loaded)
+    modprobe dummy
 
-    # Create veth pair (veth-red <--> veth-blue)
-     ip link add veth-red type veth peer name veth-blue
+    # Create first dummy interface (dummy0)
+     ip link add dummy0 type dummy
 
-    # Move interfaces to respective namespaces
-     ip link set veth-red netns red
-     ip link set veth-blue netns blue
+    # Create second dummy interface (dummy1)
+     ip link add dummy1 type dummy
 
-    # Assign IPs (different subnets for isolation)
-     ip -n red addr add 192.168.1.1/24 dev veth-red
-     ip -n blue addr add 192.168.2.1/24 dev veth-blue
+    # Assign IP to dummy0
+     ip addr add 192.168.1.1/24 dev dummy0
 
-    # Bring them up
-    ip -n red link set veth-red up
-     ip -n blue link set veth-blue up
+    # Assign IP to dummy1
+     ip addr add 192.168.1.2/24 dev dummy1
 
-    # Enable loopback in both namespaces
-     ip -n red link set lo up
-     ip -n blue link set lo up
-
-    # Ping from red to blue (should fail - no route)
-     ip netns exec red ping 192.168.2.1 -c 3
-
-    # Ping from blue to red (should fail)
-     ip netns exec blue ping 192.168.1.1 -c 3
-
+     ip link set dummy0 up
+     ip link set dummy1 up
   '';
-  netns_down = pkgs.writeShellScriptBin "inet-down" ''
+  netns_down = pkgs.writeShellScriptBin "dnet-down" ''
     # Delete namespaces (automatically removes interfaces)
-     ip netns del red
-     ip netns del blue
-  '';
+     ip link del dummy0
+     ip link del dummy1  '';
   aya_run = pkgs.writeShellScriptBin "aya-run" ''
-    RUST_LOG=info cargo run --config 'target."cfg(all())".runner="doas ip netns exec red "' -- \
-      --iface veth-red'';
+    RUST_LOG=info cargo run --config 'target."cfg(all())".runner="doas "' -- \
+      --iface $1'';
 in mkShell {
   # The Nix packages provided in the environment
   packages = with pkgs;
@@ -75,6 +62,9 @@ in mkShell {
         # Required
     echo "Welcome to nix Aya Rust Shell"
     echo "CMD:"
-    echo "    aya-run - inet-up -inet-down"
+    echo "   -> aya-run"
+    echo "   -> sudo dnet-up "
+    echo "   -> sudo dnet-down"
+    echo "dnet is dummy interface for testing"
   '';
 }
