ee727e9d5f3ed224f57487cdf42669227fb3daf0
Abdallah Ali <amaali1991@gmail.com>
2025-05-28 11:05:11 +0300

    test image 3

--- modules/nixos/archtypes/rpi/default.nix
+++ modules/nixos/archtypes/rpi/default.nix
@@ -10,23 +10,46 @@ in {
 
   config = mkIf cfg.enable {
     boot = {
-      # kernelPackages = pkgs.linuxKernel.packages.linux_rpi3;
-      kernelParams = [ "cma=320M" ];
+      kernelPackages = pkgs.linuxKernel.kernels.linux_rpi3;
+      kernelParams = [ "cma=256M" "console=ttyS1,115200n8" ];
       initrd = {
         # availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
         availableKernelModules = [ "xhci_pci" ];
-        kernelModules = [ "vc4" "bcm2835_dma" ];
+        kernelModules = [ "vc4" "bcm2835_dma" "bcm2835-v4l2" "i2c_bcm2835" ];
 
       };
       loader = {
         grub.enable = false;
         generic-extlinux-compatible.enable = true;
+        raspberryPi = {
+          firmwareConfig = ''
+            dtparam=audio=on
+            start_x=1
+            gpu_mem=256
+          '';
+          enable = true;
+          version = 3;
+          uboot.enable = true;
+        };
       };
     };
-    # services.xserver = {
-    #   enable = true;
-    #   displayManager = { lightdm = enabled; };
-    # };
+    systemd.services.btattach = {
+      before = [ "bluetooth.service" ];
+      after = [ "dev-ttyAMA0.device" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart =
+          "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA0 -P bcm -S 3000000";
+      };
+    };
+    hardware.enableRedistributableFirmware = true;
+    services = {
+      desktopManager.plasma6.enable = true;
+      xserver = {
+        enable = true;
+        displayManager.sddm.enable = true;
+      };
+    };
     networking.wireless = disabled;
     amaali7 = {
       nix = enabled;
