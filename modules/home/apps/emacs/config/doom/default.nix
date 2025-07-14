8d111885d01d21ce2e841841c941c3bf2ebf655a
Abdallah Ali <amaali1991@gmail.com>
2025-07-14 13:42:36 +0300

    emacs activation TODO:

--- modules/home/apps/emacs/config/doom/default.nix
+++ modules/home/apps/emacs/config/doom/default.nix
@@ -25,19 +25,19 @@ in {
       activation.linkDoomEmacs =
         config.lib.dag.entryAfter [ "writeBoundary" ] ''
           # Config
-          function doom_init_git {
-            ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs;
-            $HOME/.config/emacs/bin/doom install;
-          }
+          # function doom_init_git {
+          #   ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs;
+          #   $HOME/.config/emacs/bin/doom install;
+          # }
           ln -snf $HOME/.dotfiles/doom $HOME/.config/doom
-          if [ -d $HOME/.config/emacs/ ]; then
-              if [ ! -f $HOME/.config/emacs/.doomrc ]; then
-                  mv $HOME/.config/emacs $HOME/.config/emacs-old-by-hm;
-                  doom_init_git;
-              fi
-          else
-              doom_init_git;
-          fi
+          # if [ -d $HOME/.config/emacs/ ]; then
+          #     if [ ! -f $HOME/.config/emacs/.doomrc ]; then
+          #         mv $HOME/.config/emacs $HOME/.config/emacs-old-by-hm;
+          #         doom_init_git;
+          #     fi
+          # else
+          #     doom_init_git;
+          # fi
         '';
     };
   };
