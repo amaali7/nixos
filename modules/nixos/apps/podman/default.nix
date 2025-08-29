442f0c5b6548670a1d1678ef5759eb732cee0bdb
Abdallah Ali <amaali1991@gmail.com>
2025-08-29 11:11:40 +0300

    fix env

--- modules/nixos/apps/podman/default.nix
+++ modules/nixos/apps/podman/default.nix
@@ -21,7 +21,7 @@ in {
         ports = [ "127.0.0.1:1234:1234" ];
       };
     };
-    xdg.configFile."containers/registries.conf".text = ''
+    environment.etc."containers/registries.conf".text = ''
       [registries.search]
       registries = ['docker.io']
     '';
